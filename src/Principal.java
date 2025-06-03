3db6df543e4f3f9c00569b9bce9b68a2ecef681f
Cecilia Milos <cecilia.milos.22@gmail.com>
2025-06-03 13:42:06 +0200

    segundaversion

--- src/Principal.java
+++ src/Principal.java
@@ -2,8 +2,10 @@
 public class Principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		System.out.println("Hola mundo ");
+
+		System.out.println("hola a todos");
 	}
 
 }
